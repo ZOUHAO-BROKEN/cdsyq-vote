561ff79d32c83836f65a6675a02f80ea5c82eb6f
wyfvvv <5447058@qq.com>
2019-08-04 14:43:26 +0800

    new file:   src/utils/request.js

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export default {
+  prod:{
+    URL:'https://ccbynt.cn/khxxurl/web/api',
+    envVersion:'release'
+  },
+  dev:{
+    URL:'http://49.234.237.176:8000/khxxurl/web/api',
+    envVersion:'develop'
+  }
+}
