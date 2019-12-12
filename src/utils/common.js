faa5ff4792581844119a0f9fb63d67f503779baf
ZOUHAO-BROKEN <18011438527@163.com>
2019-12-12 10:26:58 +0800

    stab

--- src/utils/common.js
+++ src/utils/common.js
@@ -8,7 +8,8 @@ export function wxAPIPromise(func, obj){
 			success: resolve,
 			fail: function(err){
 				wx.hideLoading();
-				if(JSON.stringify(err).indexOf('getUserInfo:fail') == -1){
+				if((JSON.stringify(err).indexOf('getUserInfo:fail') == -1)
+					&& (JSON.stringify(err).indexOf('auth deny') == -1) ){
 					wx.showModal({
 			            title: '服务异常',
 			            showCancel: false,
